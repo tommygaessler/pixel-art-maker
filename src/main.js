183d7863240c15d8ea79eed20a2c2ebb93cc7edb
Tommy Gaessler <tommy.gaessler@gmail.com>
2016-07-22 15:51:11 -0600

    users can now enter colors

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
-  console.log('test');
 
-  var colors = ['red', 'blue', 'green'];
+  var colors = ['red', 'orange', 'yellow'];
   var isDown = false;
 
   for (var i = 0; i < 1250; i++) {
@@ -12,22 +11,61 @@ $(document).ready(function() {
     $('.colors').append('<div class="color" style="background-color:' + colors[i] + '"></div>');
   }
 
-  $('.erase').on('click', function() {
+  draw();
+
+  $('form').on('submit', function (event) {
+
+    event.preventDefault();
+
+    var newColor = $('input').val();
+
+    $('.colors').append('<div class="color" style="background-color:' + newColor + '"></div>');
+
+    $("input").val('');
+
+    colors.push(newColor);
+
+    draw();
+
+  });
+
+  $('.clear').on('click', function() {
     $('.box').css('background-color', 'white');
     $('.color').css('border', 'none');
   });
 
-  //colors, make loop
+  function draw() {
+    $('.color').on('click', function() {
+      $('.color').css('border', 'none');
+      var position = $(this).index();
+      $(this).css('border', '2px solid black');
+      console.log(position);
 
-  $('.color').on('click', function() {
+      $('.box').on('click', function() {
+        $(this).css('background-color', colors[position]);
+      });
+
+      $('.canvas > .box').mousedown(function() {
+         isDown = true;
+      })
+
+      $('.canvas > .box').mouseup(function() {
+         isDown = false;
+      });
+
+      $(".canvas > .box").mouseover(function() {
+        if(isDown) {
+          $(this).css('background-color', colors[position]);
+        }
+      });
+    });
+  }
+
+  $('.erase').on('click', function() {
     $('.color').css('border', 'none');
-    var position = $(this).index();
-    $(this).css('border', '2px solid black');
-    console.log(position);
-
 
     $('.box').on('click', function() {
-      $(this).css('background-color', colors[position]);
+      $(this).css('background-color', 'white');
     });
 
     $('.canvas > .box').mousedown(function() {
@@ -40,8 +78,11 @@ $(document).ready(function() {
 
     $(".canvas > .box").mouseover(function() {
       if(isDown) {
-        $(this).css('background-color', colors[position]);
+        $(this).css('background-color', 'white');
       }
     });
   });
+
+
+
 });
